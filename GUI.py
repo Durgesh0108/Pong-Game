28ac0865a3c19e7e959b21a7626eaf80fb7ce378
Durgesh0108 <103890880+Durgesh0108@users.noreply.github.com>
2022-09-02 18:04:47 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,34 +1,42 @@
 import os
 from tkinter import *
+
 # import main
 # import computer_main
 
 # computer_code = computer_main()
 # player_code = main()
 
+
 root = Tk()
-root.geometry("700x700")
+root.geometry("562x533")
 title = root.title("PONG GAME")
 
+BackgroundImage = PhotoImage(file="Background_image_new.png")
+# root.config(background=BackgroundImage)
+label = Label(image=BackgroundImage)
+label.place(x=0, y=0)
+
+
 def PVP_game():
-    os.system('main.py')
     root.iconify()
+    os.system('main.py')
+
 
 def PVC_game():
-    os.system('computer_main.py')
     root.iconify()
+    os.system('computer_main.py')
+
 
 PVP = PhotoImage(file="Player_vs_Player.png")
 PVC = PhotoImage(file="Player_vs_computer.png")
 
-PLayer_button = Button(root,image=PVP,command=PVP_game)
-PLayer_button.pack(padx=50,pady=80)
+PLayer_button = Button(root, image=PVP, command=PVP_game)
+# PLayer_button.place(x=180,y=80)
+PLayer_button.pack(pady=40)
 
-Computer_button = Button(root,image=PVC,command=PVC_game)
-Computer_button.pack(padx=50,pady=80)
-
-
-
-
+Computer_button = Button(root, image=PVC, command=PVC_game)
+# Computer_button.place(x=180,y=310)
+Computer_button.pack(pady=40)
 
 root.mainloop()
